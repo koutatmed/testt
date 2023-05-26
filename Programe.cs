8f425e07398d7e2354a7709e016b730523bcc471
koutatmed <koutatmed@gmail.com>
2023-05-26 13:22:24 +0200

    first commit

---
+++ Programe.cs
@@ -0,0 +1,21 @@
+using Internal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Form
+{
+    public class Programe
+    {
+        
+        List<int>   A= new List<int>();
+
+A.Add(1);
+
+
+Console.ReadKey();
+Console.Write(A);
+        }
+    
+}
